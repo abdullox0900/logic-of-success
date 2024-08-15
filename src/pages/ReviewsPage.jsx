70ac9e98e0ea01a77fd9846cfeaed58d7ced8d1c
abdullox0900 <abdullox0900@gmail.com>
2024-08-15 21:12:41 +0500

    fix: add backButton

--- src/pages/ReviewsPage.jsx
+++ src/pages/ReviewsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useTelegramColorScheme } from '../context/TelegramColorSchemeContext'
 import { reviewsData } from '../data/reviewsData'
